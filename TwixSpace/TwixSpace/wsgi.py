0d69d0c1274e2e43956d8a001f02f5bcb6eedbc0
Umair Ansari <ansariumair2402@gmail.com>
2026-03-15 07:31:47 +0530

    Fix settings module path

--- TwixSpace/TwixSpace/wsgi.py
+++ TwixSpace/TwixSpace/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TwixSpace.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TwixSpace.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TwixSpace.TwixSpace.settings')
 
 application = get_wsgi_application()
